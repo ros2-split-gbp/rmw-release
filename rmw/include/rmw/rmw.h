1d1e056408447ecab91dde295f66b5f2b1df5ab4
Chris Lalancette <clalancette@openrobotics.org>
2022-03-01 17:49:19 +0000

    Rebase from 'upstream'

--- rmw/include/rmw/rmw.h
+++ rmw/include/rmw/rmw.h
@@ -98,12 +98,14 @@ extern "C"
 #include "rosidl_runtime_c/service_type_support_struct.h"
 #include "rosidl_runtime_c/sequence_bound.h"
 
+#include "rmw/event.h"
 #include "rmw/init.h"
+#include "rmw/event_callback_type.h"
 #include "rmw/macros.h"
+#include "rmw/message_sequence.h"
 #include "rmw/publisher_options.h"
 #include "rmw/qos_profiles.h"
 #include "rmw/subscription_options.h"
-#include "rmw/message_sequence.h"
 #include "rmw/types.h"
 #include "rmw/visibility_control.h"
 
@@ -2564,10 +2566,10 @@ rmw_destroy_wait_set(rmw_wait_set_t * wait_set);
  * This function adds middleware-specific conditions to the wait set and waits
  * until one or more become ready, or until the timeout is reached.
  *
- * \remark Elapsed time is measured against the system clock.
- *   Timeout granularity is thus bound to that of the aforementioned clock and,
- *   depending on the underlying implementation, to that of platform-specific
- *   APIs to sleep and/or wait.
+ * \remark Elapsed time should be measured using a monotonic clock,
+ *   though rmw implementations could use a different one.
+ *   Timeout granularity is thus bound to that of the clock used by the underlying implementation,
+ *   and to the platform-specific APIs used to sleep and/or wait.
  *
  * \remark
  *   The amount of time this function actually waits may be either above or
@@ -2791,7 +2793,7 @@ rmw_get_node_names_with_enclaves(
  *
  * \par Thread-safety
  *   Nodes are thread-safe objects, and so are all operations on them except for finalization.
- *   Therefore, it is to query the ROS graph using the same node concurrently.
+ *   Therefore, it is safe to query the ROS graph using the same node concurrently.
  *   However, access to primitive data-type arguments is not synchronized.
  *   It is not safe to read or write `topic_name` or `count` while rmw_count_publishers()
  *   uses them.
@@ -2941,31 +2943,40 @@ rmw_compare_gids_equal(const rmw_gid_t * gid1, const rmw_gid_t * gid2, bool * re
 
 /// Check if a service server is available for the given service client.
 /**
- * This function will return true for `is_available` if there is a service
- * server available for the given client.
+ * This function checks whether one or more service services matching the
+ * given service client exist in the ROS graph, as discovered so far by the
+ * given local node.
  *
- * The node parameter must not be `NULL`, and must point to a valid node.
+ * <hr>
+ * Attribute          | Adherence
+ * ------------------ | -------------
+ * Allocates Memory   | No
+ * Thread-Safe        | Yes
+ * Uses Atomics       | Maybe [1]
+ * Lock-Free          | Maybe [1]
  *
- * The client parameter must not be `NULL`, and must point to a valid client.
+ * <i>[1] implementation defined, check implementation documentation.</i>
  *
- * The given client and node must match, i.e. the client must have been created
- * using the given node.
+ * \par Runtime behavior
+ *   To query the ROS graph is a synchronous operation.
+ *   It is also non-blocking, but it is not guaranteed to be lock-free.
+ *   Generally speaking, implementations may synchronize access to internal resources using
+ *   locks but are not allowed to wait for events with no guaranteed time bound (barring
+ *   the effects of starvation due to OS scheduling).
  *
- * The is_available parameter must not be `NULL`, and must point to a bool
- * variable.
- * The result of the check will be stored in the is_available parameter.
+ * \pre Given `node` must be a valid node, as returned by rmw_create_node().
+ * \pre Given `client` must be a valid client, as returned by rmw_create_client().
+ * \pre Given `node` must be the one the `client` was registered with.
  *
- * This function does manipulate heap memory.
- * This function is not thread-safe.
- * This function is lock-free.
- *
- * \param[in] node the handle to the node being used to query the ROS graph
- * \param[in] client the handle to the service client being queried
- * \param[out] is_available
- *   set to true if there is a service server available, else false
- * \return `RMW_RET_OK` if node the check was made successfully, or
- * \return `RMW_RET_INCORRECT_RMW_IMPLEMENTATION` if the `publisher` implementation
- *   identifier does not match this implementation, or
+ * \param[in] node Node to query the ROS graph.
+ * \param[in] client Service client to look for matching service servers.
+ * \param[out] is_available True if there is a service server available, else false.
+ * \return `RMW_RET_OK` if successful, or
+ * \return `RMW_RET_INVALID_ARGUMENT` if `node` is `NULL`, or
+ * \return `RMW_RET_INVALID_ARGUMENT` if `client` is `NULL`, or
+ * \return `RMW_RET_INVALID_ARGUMENT` if `is_available` is `NULL`, or
+ * \return `RMW_RET_INCORRECT_RMW_IMPLEMENTATION` if the implementation
+ *   identifier of `node` or `client` does not match this implementation, or
  * \return `RMW_RET_ERROR` if an unspecified error occurs.
  */
 RMW_PUBLIC
@@ -2985,6 +2996,146 @@ RMW_PUBLIC
 RMW_WARN_UNUSED
 rmw_ret_t
 rmw_set_log_severity(rmw_log_severity_t severity);
+
+/// Set the on new message callback function for the subscription.
+/**
+ * This API sets the callback function to be called whenever the
+ * subscription is notified about a new message.
+ *
+ * This callback is called for each new message received by the subscription.
+ * If messages arrive before the callback is registered, the number_of_events
+ * argument given to the callback may be > 1.
+ *
+ * The callback may be called from a thread that the rmw implementation
+ * created, rather than a thread owned by the user, i.e. some thread other
+ * than user owned threads calling rmw functions such as rmw_wait() or
+ * rmw_publish().
+ *
+ * This function is thread-safe.
+ * This is required of the rmw implementation because the callback may be called
+ * from any middleware thread, and this function could be called by the user
+ * at any time.
+ *
+ * \param[in] subscription The subscription on which to set the callback
+ * \param[in] callback The callback to be called when new messages arrive
+ * \param[in] user_data Given to the callback when called later, may be NULL
+ * \return `RMW_RET_OK` if successful, or
+ * \return `RMW_RET_INVALID_ARGUMENT` if `subscription` or `callback` is NULL, or
+ * \return `RMW_RET_UNSUPPORTED` if the API is not implemented in the dds implementation
+ */
+RMW_PUBLIC
+RMW_WARN_UNUSED
+rmw_ret_t
+rmw_subscription_set_on_new_message_callback(
+  rmw_subscription_t * subscription,
+  rmw_event_callback_t callback,
+  const void * user_data);
+
+/// Set the on new request callback function for the service.
+/**
+ * This API sets the callback function to be called whenever the
+ * service is notified about a new request.
+ *
+ * This callback is called for each new request received by the service.
+ * If requests arrive before the callback is registered, the number_of_events
+ * argument given to the callback may be > 1.
+ *
+ * The callback may be called from a thread that the rmw implementation
+ * created, rather than a thread owned by the user, i.e. some thread other
+ * than user owned threads calling rmw functions such as rmw_wait() or
+ * rmw_send_request().
+ *
+ * This function is thread-safe.
+ * This is required of the rmw implementation because the callback may be called
+ * from any middleware thread, and this function could be called by the user
+ * at any time.
+ *
+ * \param[in] service The service on which to set the callback
+ * \param[in] callback The callback to be called when new requests arrive
+ * \param[in] user_data Given to the callback when called later, may be NULL
+ * \return `RMW_RET_OK` if callback was set to the listener, or
+ * \return `RMW_RET_INVALID_ARGUMENT` if `service` or `callback` is NULL, or
+ * \return `RMW_RET_UNSUPPORTED` if the API is not implemented in the dds implementation
+ */
+RMW_PUBLIC
+RMW_WARN_UNUSED
+rmw_ret_t
+rmw_service_set_on_new_request_callback(
+  rmw_service_t * service,
+  rmw_event_callback_t callback,
+  const void * user_data);
+
+/// Set the on new response callback function for the client.
+/**
+ * This API sets the callback function to be called whenever the
+ * client is notified about a new response.
+ *
+ * This callback is called for each new response received by the client.
+ * If responses arrive before the callback is registered, the number_of_events
+ * argument given to the callback may be > 1.
+ *
+ * The callback may be called from a thread that the rmw implementation
+ * created, rather than a thread owned by the user, i.e. some thread other
+ * than user owned threads calling rmw functions such as rmw_wait() or
+ * rmw_take_response().
+ *
+ * This function is thread-safe.
+ * This is required of the rmw implementation because the callback may be called
+ * from any middleware thread, and this function could be called by the user
+ * at any time.
+ *
+ * \param[in] client The client on which to set the callback
+ * \param[in] callback The callback to be called when new responses arrive
+ * \param[in] user_data Given to the callback when called later, may be NULL
+ * \return `RMW_RET_OK` if callback was set to the listener, or
+ * \return `RMW_RET_INVALID_ARGUMENT` if `client` or `callback` is NULL, or
+ * \return `RMW_RET_UNSUPPORTED` if the API is not implemented in the dds implementation
+ */
+RMW_PUBLIC
+RMW_WARN_UNUSED
+rmw_ret_t
+rmw_client_set_on_new_response_callback(
+  rmw_client_t * client,
+  rmw_event_callback_t callback,
+  const void * user_data);
+
+/// Set the callback function for the event.
+/**
+ * This API sets the callback function to be called whenever the
+ * event is notified about a new instance of the event.
+ *
+ * For example, this could be called when incompatible QoS is detected, or
+ * a deadline is missed, or any other QoS event.
+ *
+ * This callback is called for each new event that occurs for this rmw_event_t
+ * instance.
+ * If events occur before the callback is registered, the number_of_events
+ * argument given to the callback may be > 1.
+ *
+ * The callback may be called from a thread that the rmw implementation
+ * created, rather than a thread owned by the user, i.e. some thread other
+ * than user owned threads calling rmw functions such as rmw_wait() or
+ * rmw_publish().
+ *
+ * This function is thread-safe.
+ * This is required of the rmw implementation because the callback may be called
+ * from any middleware thread, and this function could be called by the user
+ * at any time.
+ *
+ * \param[in] event The event on which to set the callback
+ * \param[in] callback The callback to be called when new events occur
+ * \param[in] user_data Given to the callback when called later, may be NULL
+ * \return `RMW_RET_OK` if callback was set to the listener, or
+ * \return `RMW_RET_INVALID_ARGUMENT` if `event` or `callback` is NULL, or
+ * \return `RMW_RET_UNSUPPORTED` if the API is not implemented in the dds implementation
+ */
+RMW_PUBLIC
+RMW_WARN_UNUSED
+rmw_ret_t
+rmw_event_set_callback(
+  rmw_event_t * event,
+  rmw_event_callback_t callback,
+  const void * user_data);
 
 #ifdef __cplusplus
 }
